83b0e9e63ffa6ace6fb799493012143173b63fe0
Shahabuddin1122 <shavoddin54@gmail.com>
2024-11-16 22:44:00 +0600

    integrated top queries

--- frontend/src/app/chat-overview/page.tsx
+++ frontend/src/app/chat-overview/page.tsx
@@ -1,48 +1,54 @@
+"use client";
 
 import Sidebar from "@/components/sidebar";
 import Header from "@/components/header";
-import axios from "axios";
+import useSWR from "swr";
+import { fetcher } from "@/utils/fetcher";
 
-const page = () => {
-    axios.get('http://localhost:8000/manager/get-top-queries')
-        .then((res)=>{
-            console.log(res)
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
+
+const Page = () => {
+    const { data, error, isLoading } = useSWR<QueriesType[]>("http://localhost:8000/manager/get-top-queries", fetcher, { refreshInterval: 1000 });
+
+
     return (
-        <>
-            <div className={'bg-secondary flex max-w-[2000px] mx-auto w-11/12'}>
-                <Sidebar/>
-                <div className={'w-4/5 h-screen pl-10 pt-10 pr-2'}>
-                    <Header/>
-                    <div className={'py-4 text-white'}>
-                        <h1 className={'text-xl font-bold'}>Top User Prompts</h1>
-                        <div className="overflow-x-auto py-4">
-                            <table className="min-w-full border border-gray-300">
-                                <thead>
-                                <tr className="bg-primary">
-                                    <th className="px-4 py-2 border text-center">No</th>
-                                    <th className="px-4 py-2 border text-left">Prompt</th>
-                                </tr>
-                                </thead>
-                                <tbody>
+        <div className={'bg-secondary flex max-w-[2000px] mx-auto w-11/12'}>
+            <Sidebar />
+            <div className={'w-4/5 h-screen pl-10 pt-10 pr-2'}>
+                <Header />
+                <div className={'py-4 text-white'}>
+                    <h1 className={'text-xl font-bold'}>Top User Prompts</h1>
+                    <div className="overflow-x-auto py-4">
+                        <table className="min-w-full border border-gray-300">
+                            <thead>
+                            <tr className="bg-primary">
+                                <th className="px-4 py-2 border text-center">No</th>
+                                <th className="px-4 py-2 border text-left">Prompt</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {isLoading && (
                                 <tr>
-                                    <td className="px-4 py-2 border text-center">1</td>
-                                    <td className="px-4 py-2 border">Sample prompt text here</td>
+                                    <td colSpan={2} className="px-4 py-2 border text-center">Loading...</td>
                                 </tr>
+                            )}
+                            {error && (
                                 <tr>
-                                    <td className="px-4 py-2 border text-center">2</td>
-                                    <td className="px-4 py-2 border">Another prompt text example</td>
+                                    <td colSpan={2} className="px-4 py-2 border text-center">Error loading data</td>
                                 </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                            )}
+                            {!isLoading && !error && data && (data || []).map((query, index) => (
+                                <tr key={query._id}>
+                                    <td className="px-4 py-2 border text-center">{index + 1}</td>
+                                    <td className="px-4 py-2 border">{query.name}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
-        </>
-    )
-}
-export default page;
+        </div>
+    );
+};
+
+export default Page;
---
+++ frontend/src/utils/data.d.ts
@@ -0,0 +1,5 @@
+interface QueriesType {
+    _id: string;
+    name: string;
+    count: string;
+}
