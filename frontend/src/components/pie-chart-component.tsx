b5272bc6b16fd52eb11affd80c32eb6f73c60680
Shahabuddin1122 <shavoddin54@gmail.com>
2024-11-18 23:57:50 +0600

    integrate delete api

--- frontend/src/components/pie-chart-component.tsx
+++ frontend/src/components/pie-chart-component.tsx
@@ -1,15 +1,40 @@
+"use client"
 import Image from "next/image";
 import PieChart from "@/components/piechart";
+import axios, {AxiosRequestConfig} from "axios";
+import {baseUrl} from "@/utils/constant";
 
 const PieChartComponent = ({data}: {data: QuestionData}) => {
     const optionsArray = data.options;
     const voteCountsArray = optionsArray.map(option => data.option_counts[option]);
 
+    const delete_pole = async ()=>{
+        const val = {
+            "question": data.question
+        }
+        const config: AxiosRequestConfig = {
+            data: val
+        };
+        await axios.delete(`${baseUrl}/manager/get-pole`, config)
+            .then((res)=>{
+                console.log(res)
+            })
+            .catch((err)=>{
+                console.error(err)
+            })
+    }
     return (
         <>
             <div className={'lg:w-2/5 w-full bg-primary rounded-xl relative'}>
                 <p className={'text-white font-semibold m-4 w-5/6'}>{data.question}</p>
-                <Image src={'/cross.svg'} alt={'cross'} height={10} width={20} className={'absolute top-2 right-2'}/>
+                <Image
+                    src={'/cross.svg'}
+                    alt={'cross'}
+                    height={10}
+                    width={20}
+                    className={'absolute top-2 right-2'}
+                    onClick={delete_pole}
+                />
                 <div className={'w-full flex justify-center'}>
                     <PieChart labels={optionsArray} value={voteCountsArray}/>
                 </div>
